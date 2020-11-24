fd1aba09085cb3f69511715b8d0623bdd1d80c1d
siqisiqisiqi <siqizhen@usc.edu>
2020-11-24 16:32:28 +0800

    delete matlab code

--- matlab_code/angletoindex.m
+++
@@ -1,8 +0,0 @@
-function index = angletoindex(angle)
-    Num_degree = 36;
-    angle_list = -90:(180/Num_degree):89;
-    if angle >= 90
-        angle = angle - 180;
-    end
-    [~, index] = min(abs(angle_list - angle));
-end
